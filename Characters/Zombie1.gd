3e45d407087139cff166880bf2bf0b948c32857b
shanem1996 <56543705+shanem1996@users.noreply.github.com>
2021-02-24 17:56:22 +0000

    Updated Zombie1 Script

--- Characters/Zombie1.gd
+++ Characters/Zombie1.gd
@@ -1,16 +1,54 @@
-extends KinematicBody2D
+extends "res://Characters/CharacterTemplate.gd"
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	var velocity = Vector2.ZERO
+	
+	if Input.is_action_pressed("player_down"):
+		velocity.y += 1
+		
+	if Input.is_action_pressed("player_up"):
+		velocity.y -= 1
+	
+	if Input.is_action_pressed("player_left"):
+		velocity.x -= 1
+		
+	if Input.is_action_pressed("player_right"):
+		velocity.x += 1	
+		
+	print(velocity)
+		
+	
+		
+	move_and_slide(velocity.normalized() * SPEED)	
+	
+	player_animation(velocity)
+	
+func player_animation(velocity):
+	if velocity.y < 0  and velocity.x > 0:
+		$AnimatedSprite.play("Walk_Diagonal_Up")
+		$AnimatedSprite.flip_h = false
+		
+	elif velocity.y < 0  and velocity.x < 0:
+		$AnimatedSprite.play("Walk_Diagonal_Up")
+		$AnimatedSprite.flip_h = true
+		
+	elif velocity.y < 0:
+		$AnimatedSprite.play("Walk_Up")
+		
+	elif velocity.y > 0  and velocity.x > 0:
+		$AnimatedSprite.play("Walk_Diagonal_Down")
+		$AnimatedSprite.flip_h = false
+		
+	elif velocity.y > 0  and velocity.x < 0:
+		$AnimatedSprite.play("Walk_Diagonal_Down")
+		$AnimatedSprite.flip_h = true
+		
+	elif velocity.y > 0:
+		$AnimatedSprite.play("Walk_Down")
+		
+	elif velocity.x != 0:
+		$AnimatedSprite.play("Walk_Side")
+		$AnimatedSprite.flip_h = velocity.x < 0
+		
+	else:
+		$AnimatedSprite.play("Idle")
