5e5eae79518a3630a8eb56697cf0b552265126b5
shanem1996 <56543705+shanem1996@users.noreply.github.com>
2021-03-04 10:29:54 +0000

    Created building scene

--- Level/Level.gd
+++ Level/Level.gd
@@ -5,31 +5,25 @@ const ZOMBIE = preload("res://Characters/Enemies/Zombie_1/Zombie1.tscn")
 
 func _ready():
 	$YSort/Player.position = $PlayerStartPos.position
+	
+#	for i in range (20):
+#		var new_zombie = zombie.instance()
+#
+#		new_zombie.global_position.x = rand_range(0,400)
+#		new_zombie.global_position.y = rand_range(0,400)
+#
+#		add_child(new_zombie)
 
 func zombie_spawn(start_pos):
 	var zombie = ZOMBIE.instance()
 	var zombie_start_node_name = "ZombieStartPos/StartPos" + str(start_pos)
 	zombie.position = get_node(zombie_start_node_name).position
 	$YSort.add_child(zombie)
-	
-	var timer_node_name = "ZombieSpawnTimers/Timer" + str(start_pos)
-	get_node(timer_node_name).wait_time = rand_range(1.5,2.5)
 
 func _on_Timer1_timeout():
-	zombie_spawn(1)
+	
+	get_node("ZombieSpawnTimer/Timer1").wait_time = (rand_range(1,4))
+	zombie_spawn(round(rand_range(1,5)))
 
+	
 
-func _on_Timer2_timeout():
-	zombie_spawn(2)
-
-
-func _on_Timer3_timeout():
-	zombie_spawn(3)
-
-
-func _on_Timer4_timeout():
-	zombie_spawn(4)
-
-
-func _on_Timer5_timeout():
-	zombie_spawn(5)
---
+++ Main Menu/Buttons/QuitGameButton.gd
@@ -0,0 +1,6 @@
+extends Button
+
+
+
+func _on_QuitGameButton_pressed():
+	get_tree().quit()
