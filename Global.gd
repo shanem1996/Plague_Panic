9fc98829849c2ffc7f172940504233d74e621599
Sean <56120169+Fult0@users.noreply.github.com>
2021-03-31 18:16:53 +0100

    Start on Rounds System

--- Global.gd
+++ Global.gd
@@ -1,4 +1,3 @@
 extends Node
 
 var player_pos = Vector2.ZERO
-var round_timer = 0
--- Level/Level.gd
+++ Level/Level.gd
@@ -5,22 +5,15 @@ const ZOMBIE = preload("res://Characters/Enemies/Zombie_1/Zombie1.tscn")
 func _ready():
 	var healthBar = $"UI/healthBar"
 	var roundLabel = $"UI/roundLabel"
+	var current_round = 0
 	$YSort/Player.position = $PlayerStartPos.position
 	roundLabel.visible = false
-	newRound()
+	newRound(roundLabel,current_round)
 	
 func _process(delta):
 	var roundLabel = $"UI/roundLabel"
 	var player_pos = $"YSort/Player".position
 	Global.player_pos = player_pos
-	roundLabel.text = "Round Starting : " + str(Global.round_timer)
-	
-	if Global.round_timer <= 3:
-		roundLabel.visible = true
-	
-	if Global.round_timer <= 0:
-		$"ZombieSpawnTimer/LabelTimer".stop()
-		roundLabel.text = "Round 1"
 		
 
 func zombie_spawn(start_pos):
@@ -29,20 +22,21 @@ func zombie_spawn(start_pos):
 	zombie.position = get_node(zombie_start_node_name).position
 	$YSort.add_child(zombie)
 
-func _on_RoundStartTimer_timeout():
-	$"ZombieSpawnTimer/SpawnTimer".start()
-
 func _on_SpawnTimer_timeout():
 	get_node("ZombieSpawnTimer/SpawnTimer").wait_time = (rand_range(1,4))
 	zombie_spawn(round(rand_range(1,5)))
 	
-func _on_LabelTimer_timeout():
-	Global.round_timer -= 1
+func newRound(roundLabel,current_round):
+	roundLabel.visible = true
+	var counter = 6
+	for i in range(5,-1,-1):
+		roundLabel.text = "Round Starting: "+ str(i)
+		counter -= 1
+		print(counter)
+		if counter == 0:
+			roundLabel.text = "Round: " + str(current_round + 1)
+			$"ZombieSpawnTimer/SpawnTimer".start()
+		yield(get_tree().create_timer(1), "timeout")
 	
-func newRound():
-	Global.round_timer = 3
+		
 	
-
-func _on_Player_max_health(healthBar):
-	var current_health = $"YSort/Player".health
-	healthBar.set_value(current_health)
