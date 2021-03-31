cd660b13b7466bcdc4aeeaa293faf07e218a4f72
Sean <56120169+Fult0@users.noreply.github.com>
2021-03-31 19:01:50 +0100

    Round System Update

--- Global.gd
+++ Global.gd
@@ -1,3 +1,5 @@
 extends Node
 
 var player_pos = Vector2.ZERO
+var current_round = 0
+var roundLabel
--- Level/Level.gd
+++ Level/Level.gd
@@ -4,16 +4,19 @@ const ZOMBIE = preload("res://Characters/Enemies/Zombie_1/Zombie1.tscn")
 
 func _ready():
 	var healthBar = $"UI/healthBar"
-	var roundLabel = $"UI/roundLabel"
-	var current_round = 0
+	Global.roundLabel = $"UI/roundLabel"
+	Global.current_round = 0
 	$YSort/Player.position = $PlayerStartPos.position
-	roundLabel.visible = false
-	newRound(roundLabel,current_round)
+	Global.roundLabel.visible = false
+	newRound(Global.roundLabel,Global.current_round)
 	
 func _process(delta):
 	var roundLabel = $"UI/roundLabel"
 	var player_pos = $"YSort/Player".position
 	Global.player_pos = player_pos
+	
+	if Input.is_action_just_pressed("ui_up"):
+		newRound(Global.roundLabel,Global.current_round)
 		
 
 func zombie_spawn(start_pos):
@@ -21,22 +24,20 @@ func zombie_spawn(start_pos):
 	var zombie_start_node_name = "ZombieStartPos/StartPos" + str(start_pos)
 	zombie.position = get_node(zombie_start_node_name).position
 	$YSort.add_child(zombie)
-
-func _on_SpawnTimer_timeout():
-	get_node("ZombieSpawnTimer/SpawnTimer").wait_time = (rand_range(1,4))
-	zombie_spawn(round(rand_range(1,5)))
 	
 func newRound(roundLabel,current_round):
-	roundLabel.visible = true
+	Global.roundLabel.visible = true
 	var counter = 6
-	for i in range(5,-1,-1):
-		roundLabel.text = "Round Starting: "+ str(i)
+	for i in range(5,0,-1):
+		Global.roundLabel.text = "Round Starting: "+ str(i)
 		counter -= 1
-		print(counter)
-		if counter == 0:
-			roundLabel.text = "Round: " + str(current_round + 1)
-			$"ZombieSpawnTimer/SpawnTimer".start()
 		yield(get_tree().create_timer(1), "timeout")
+	if counter == 1:
+			Global.current_round += 1
+			Global.roundLabel.text = "Round: " + str(Global.current_round)
+			for j in range(0,Global.current_round*10,+1):
+				zombie_spawn(round(rand_range(1,12)))
+				yield(get_tree().create_timer(rand_range(1,4)), "timeout")
 	
 		
 	
