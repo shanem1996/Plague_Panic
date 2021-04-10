10437d819ed621ee1d318d7423b296f104c37ad0
Sean <56120169+Fult0@users.noreply.github.com>
2021-04-10 21:29:14 +0100

    bullets now have a lifetime of 4 seconds

--- Weapons/Bullet/Bullet.gd
+++ Weapons/Bullet/Bullet.gd
@@ -4,8 +4,12 @@ var bullet_speed = 400
 
 func _ready():
 	apply_impulse(Vector2(),Vector2(bullet_speed,0).rotated(rotation))
+	get_node("Lifetime").start()
 
 func _on_Bullet_body_entered(body):
 	if body.is_in_group("zombie"):
 		body.takeDamage()
+	queue_free()
+
+func _on_Lifetime_timeout():
 	queue_free()
